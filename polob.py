a45db55580bc8279cec9d324745024093aeacfec
Ong Teck Wu <ongteckwu@hotmail.com>
2016-12-30 23:02:31 +0800

    restarts when timeout

--- polob.py
+++ polob.py
@@ -1,63 +1,101 @@
 from poloniex import Poloniex
 from datetime import datetime
 import time
+import inspect
+import requests
+
 """
 Poloniex bot that buys 0.001 EXP every X secs
 """
-
 SECS = 60
 AMT = 1
 CURRENCY = "EXP"
 MIN_BALANCE = AMT
 TICKER = 'BTC_{currency}'.format(currency=CURRENCY)
+POLO_TIME_OUT = 5
+TIME_OUT_RETRY_TIME = 1
+
+
+def classDecorator(decorator):
+    def decorate(cls):
+        for name, fn in inspect.getmembers(cls, inspect.ismethod):
+            setattr(cls, name, decorator(fn))
+        return cls
+    return decorate
+
+
+def timeOutExceptionDecorator(func):
+    def f(*args, **kwargs):
+        global TIME_OUT_RETRY_TIME
+        global main
+        try:
+            out = func(*args, **kwargs)
+        except requests.exceptions.Timeout as e:
+            print(e.message)
+            print("Retrying in {sec} secs".format(sec=TIME_OUT_RETRY_TIME))
+            time.sleep(TIME_OUT_RETRY_TIME)
+            main()
+        return out
+
+    return f
 
 # polo
-polo = Poloniex(extend=True)
+newPoloniex = classDecorator(timeOutExceptionDecorator)(Poloniex)
+polo = newPoloniex(extend=True, timeout=POLO_TIME_OUT)
 # API KEY
-polo.Key = 1111  # to be filled
-polo.Secret = 11111  # to be filled
-
-TIME = time.time() - SECS
-# for checking the balance in the first iteration
-currentPrice = polo.returnOrderBook(TICKER, depth=5)["asks"][0][0]
-
-balance = polo.returnBalances()
-if "error" in balance:
-    print(balance["error"])
-    exit()
-else:
-    print("Your BTC balance is {amt}".format(amt=balance["BTC"]))
-
-while True:
-    if time.time() - TIME >= SECS:
-        # check balance
-        balanceInBTC = polo.returnBalances()["BTC"]
-        # if amount of BTC cannot buy AMT * 10 coins, don't buy
-        if float(balanceInBTC) < MIN_BALANCE * float(currentPrice):
-            print("Insufficient balance in BTC to buy {amt} {cur}. Balance: {bal}. Price of {cur}: {price}".format(
-                bal=balanceInBTC, cur=CURRENCY,
-                price=currentPrice, amt=MIN_BALANCE))
-        else:
-            isBought = False
-            # while order hasnt gone through
-            while not isBought:
-                currentPrice = polo.returnOrderBook(
-                    TICKER, depth=5)["asks"][0][0]
+polo.Key = "Y0E1JVKD-PF0RCBVE-I4OO8K28-YJIHIQJ7"  # to be filled
+polo.Secret = "c31279732b0a375c6645e05a8f7233f3c883198a8195e8f002418258a50152fd4342f8b2db2f1ac771d31fcad154ab61499debf9b9fba69e6e690ce678da1002"  # to be filled
 
 
-                order = polo.buy(TICKER, currentPrice, AMT)
-                print(order)
+def main():
+    """
+    main function to run
+    """
+    TIME = time.time() - SECS
 
-                # if order goes through
-                if "orderNumber" in order.keys():
-                    # get current datetime
-                    dt = datetime.utcnow().strftime(
-                        "%y-%m-%d %H:%M:%S {tz}".format(tz=time.tzname[0]))
+    balance = polo.returnBalances()
+    if "error" in balance:
+        print(balance["error"])
+        exit()
+    else:
+        print("Your BTC balance is {amt}".format(amt=balance["BTC"]))
 
-                    print("{amt} {cur} has been bought @ {price} || {dt}".format(
-                        amt=AMT, cur=CURRENCY, price=currentPrice, dt=dt))
-                    # order goes through
-                    isBought = True
+    # for checking the balance in the first iteration
+    currentPrice = polo.returnOrderBook(TICKER, depth=5)["asks"][0][0]
 
-        # reset timer
-        TIME = time.time()
+    while True:
+        if time.time() - TIME >= SECS:
+            # check balance
+            balanceInBTC = polo.returnBalances()["BTC"]
+            # if amount of BTC cannot buy AMT * 10 coins, don't buy
+            if float(balanceInBTC) < MIN_BALANCE * float(currentPrice):
+                print("Insufficient balance in BTC to buy {amt} {cur}. Balance: {bal}. Price of {cur}: {price}".format(
+                    bal=balanceInBTC, cur=CURRENCY,
+                    price=currentPrice, amt=MIN_BALANCE))
+            else:
+                isBought = False
+                # while order hasnt gone through
+                while not isBought:
+                    currentPrice = polo.returnOrderBook(
+                        TICKER, depth=5)["asks"][0][0]
+
+                    order = polo.buy(TICKER, currentPrice, AMT)
+                    print(order)
+
+                    # if order goes through
+                    if "orderNumber" in order.keys():
+                        # get current datetime
+                        dt = datetime.utcnow().strftime(
+                            "%y-%m-%d %H:%M:%S {tz}".format(tz=time.tzname[0]))
+
+                        print("{amt} {cur} has been bought @ {price} || {dt}".format(
+                            amt=AMT, cur=CURRENCY, price=currentPrice, dt=dt))
+                        # order goes through
+                        isBought = True
+
+            # reset timer
+            TIME = time.time()
+
+
+if __name__ == "__main__":
+    main()
